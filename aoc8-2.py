82f9838898d4a9b14ef65f8242259f6c7c379ed0
trizkut <trizkut@gmail.com>
2020-12-08 02:08:58 -0500

    added day 8

---
+++ aoc8-2.py
@@ -0,0 +1,40 @@
+
+instrList = []
+
+for line in open('inputs/input8.txt', 'r'):
+    instr = line.strip().split(" ")
+    instrList.append(instr)
+
+
+
+
+for i in range(0, len(instrList)):
+
+    accumulator = 0
+    index = 0
+    dupe = 0
+    executedList = [False] * len(instrList)
+    
+    while (index < len(instrList)):
+        curInstr = instrList[index][0]
+        if (index == i):
+            if (curInstr == 'nop'): curInstr = 'jmp'
+            if (curInstr == 'jmp'): curInstr = 'nop'
+        if (executedList[index]):
+            print('Duplicate Execution, Terminating;  Accumulator Value: ', accumulator)
+            dupe = 1
+            break
+        executedList[index] = True
+        if (curInstr == 'nop'):
+            index += 1
+            continue
+        if (curInstr == 'acc'):
+            accumulator += int(instrList[index][1])
+        if (curInstr == 'jmp'):
+            index += int(instrList[index][1])
+        else:
+            index += 1
+
+    if (not dupe): #if no duplicate execution after loop, this is the right instruction to replace
+        print('Successful execution; Accumulator value: ', accumulator)
+        break
---
+++ aoc8.py
@@ -0,0 +1,26 @@
+accumulator = 0
+index = 0
+instrList = []
+
+for line in open('inputs/input8.txt', 'r'):
+    instr = line.strip().split(" ")
+    instrList.append(instr)
+
+executedList = [False] * len(instrList)
+
+
+while (index < len(instrList)):
+
+    if (executedList[index]):
+        print('Duplicate Execution, Terminating;  Accumulator Value: ', accumulator)
+        break
+    executedList[index] = True
+    if (instrList[index][0] == 'nop'):
+        index += 1
+        continue
+    if (instrList[index][0] == 'acc'):
+        accumulator += int(instrList[index][1])
+    if (instrList[index][0] == 'jmp'):
+        index += int(instrList[index][1])
+    else:
+        index += 1
